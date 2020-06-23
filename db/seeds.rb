214ee32462edd22091e310b38940f77868b52772
Chris Kalfas <christopherkalfas22@gmail.com>
2020-06-23 12:04:13 -0400

    Laid out models, assocs and some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = User.create([{name: "Chris"}, {name:"Steve"}, {name:"Brian"}])
+
+groups = Group.create([{name:"Roswell's Finest"}, {name:"Average Joes"}])
+
+membership = Membership.create([{user_id: 1, group_id: 1}])
